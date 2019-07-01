73d01191d2c489e5c250aa9ab091f189ab6abd9e
Your name <Your email>
2019-07-01 10:38:32 -0700

    Updates to materialize app.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require materialize
 //= require activestorage
 //= require turbolinks
+//= require main
 //= require_tree .
